2eff6ac4efa320bd81a86e70a947b74c68a59ca9
Anurag021 <anu.rawat@outlook.com>
2022-02-20 19:46:07 +0530

    Updated Code for Cart and Whish List
    Added Code for cart and whishlist page, and added css code.

--- app.js
+++ app.js
@@ -5,3 +5,31 @@ passwordEyeBtn.addEventListener('click', () => {
     const type = passwordForm.getAttribute('type') === 'password' ? 'text' : 'password';
     passwordForm.setAttribute('type',type);
 })
+
+// Functionality for Cart Quantity inc and dec.
+
+const optionDec = document.querySelectorAll('.cart-option-dec')
+const optionInc = document.querySelectorAll('.cart-option-inc')
+const optionNo = document.querySelectorAll('.cart-quantity-no')
+let value = 0;
+for (let i = 0; i < optionNo.length; i++) {
+    optionInc[i].addEventListener('click', ()=>{
+        if(Number(optionNo[i].value) < Number(optionNo[i].max) ){
+            optionNo[i].value= Number(optionNo[i].value)+1
+        }})
+    
+    optionDec[i].addEventListener('click', ()=>{
+        if(Number(optionNo[i].value) > Number(optionNo[i].min) ){
+            optionNo[i].value= Number(optionNo[i].value)-1
+        }})
+}
+
+// Close card functionality
+const cardClose = document.querySelectorAll(".card-close")
+
+for (let i = 0; i < cardClose.length; i++) {
+    console.log('close click trigger');
+    cardClose[i].addEventListener('click', () => {
+        cardClose[i].parentElement.style.display = 'none';
+    })
+}
