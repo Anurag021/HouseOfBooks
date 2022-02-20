f8b91ec1ca2adaf25dbcce9358ebe507095d4ade
Anurag021 <anu.rawat@outlook.com>
2022-02-20 15:15:58 +0530

    Updated Code For Cart and WishList
    Updated Code For Cart and WishList

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-var passwordEyeBtn = document.querySelector('.password-check')
-var passwordForm = document.querySelector('.input-password')
+const passwordEyeBtn = document.querySelector('.password-check')
+const passwordForm = document.querySelector('.input-password')
 
 passwordEyeBtn.addEventListener('click', () => {
     const type = passwordForm.getAttribute('type') === 'password' ? 'text' : 'password';
     passwordForm.setAttribute('type',type);
 })
+
+// Functionality for Cart Quantity inc and dec.
+
+
+const optionDec = document.querySelectorAll('.cart-option-dec')
+const optionInc = document.querySelectorAll('.cart-option-inc')
+const optionNo = document.querySelectorAll('.cart-quantity-no')
+let value = 0;
+for (let i = 0; i < optionNo.length; i++) {
+    optionInc[i].addEventListener('click', ()=>{
+        if(Number(optionNo[i].value) < Number(optionNo[i].max) ){
+            optionNo[i].value= Number(optionNo[i].value)+1
+        }})
+    
+    optionDec[i].addEventListener('click', ()=>{
+        if(Number(optionNo[i].value) > Number(optionNo[i].min) ){
+            optionNo[i].value= Number(optionNo[i].value)-1
+        }})
+    
+}
+
